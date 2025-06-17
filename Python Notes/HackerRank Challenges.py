dc73c0a926490b18848d5464d7cb767306c9f2eb
Bluey <BlueyVuitton713@gmail.com>
2025-06-16 23:29:10 -0700

    Started tracking 6.16.25

---
+++ Python Notes/HackerRank Challenges.py
@@ -0,0 +1,18 @@
+#if the inputs are unclear in a program run the following
+# import sys
+#
+# for line in sys.stdin:
+#     print(f"Received line: {line.strip()}")
+#The above will print if the inputs are one input or multiple
+#so the day 6 inputs were 3 separate inputs so i need to use 3 inputs, one for int and n for each nth string
+
+
+#Day 6 of 30 days of code
+#creat a program that takes in an int of how many words will be entere and for
+# each word, print the even and odd letter as a space separated string
+# ex. 2, hacker, rank prints hce akr rn ak
+# n = int(input())
+# for i in range(n):
+#     s = input()
+#     print(f"{s[::2]} {s[1::2]}")
+
