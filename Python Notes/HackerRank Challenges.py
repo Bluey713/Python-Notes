fab312cbf9316ba14f6f31250330f6840ac585fe
Bluey <BlueyVuitton713@gmail.com>
2025-06-17 08:43:50 -0700

    Grammar and spelling corrections

--- Python Notes/HackerRank Challenges.py
+++ Python Notes/HackerRank Challenges.py
@@ -4,11 +4,11 @@
 # for line in sys.stdin:
 #     print(f"Received line: {line.strip()}")
 #The above will print if the inputs are one input or multiple
-#so the day 6 inputs were 3 separate inputs so i need to use 3 inputs, one for int and n for each nth string
+#so the day 6 inputs were 3 separate inputs, so I need to use 3 inputs, one for int and n for each nth string
 
 
 #Day 6 of 30 days of code
-#creat a program that takes in an int of how many words will be entere and for
+#creat a program that takes in an int of how many words will be entered and for
 # each word, print the even and odd letter as a space separated string
 # ex. 2, hacker, rank prints hce akr rn ak
 # n = int(input())
