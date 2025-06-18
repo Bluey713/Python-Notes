7e43bec425cdcf79f1079ebb729a650de3e8ace6
Bluey <BlueyVuitton713@gmail.com>
2025-06-17 20:29:48 -0700

    function keywords

--- Python Notes/Functions.py
+++ Python Notes/Functions.py
@@ -1,1 +1,10 @@
 #I understund the basics of functions but never understood args and kwargs
+def testing_keyword(test3, test1="name", test2="last name"):
+    print(test3)
+    print(test1)
+    print(test2)
+
+testing_keyword(1000)
+testing_keyword(test3="James")
+testing_keyword(test3 = "New name", test1="This is the new string")
+
