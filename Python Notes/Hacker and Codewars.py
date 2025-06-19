6f49fa20f5dd6eff66ea14b6f534e2b2c5f753d1
Bluey <BlueyVuitton713@gmail.com>
2025-06-18 20:42:07 -0700

    Created a new file for all my solved challenges

---
+++ Python Notes/Hacker and Codewars.py
@@ -0,0 +1,25 @@
+#creat a function that sums the points based on the given values
+def alphabet_war(fight):
+    left_values = {"w": 4, "p": 3, "b": 2, "s": 1}
+    left_score = 0
+    right_values = {"m": 4, "q": 3, "d": 2, "z": 1}
+    right_score = 0
+
+    for letter in fight:
+        # if letter in left_values.keys():
+        #     left_score += left_values[letter]
+        # elif letter in right_values.keys():
+        #     right_score += right_values[letter]
+        #The above 4 lines are my initial code
+        #I saw the below 2 lines to make my code shorter which gets rid of the if statement.
+        left_score += left_values.get(letter, 0)
+        right_score += right_values.get(letter, 0)
+
+    if left_score > right_score:
+        print("Left side wins!")
+    elif right_score > left_score:
+        print("Right side wins!")
+    else:
+        print("Let's fight again!")
+
+alphabet_war("pqd")
