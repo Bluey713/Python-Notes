a6c2a6d6a418247cb1d651b4be495ebb3e0ec174
Bluey Blue <blueyvuitton713@gmail.com>
2025-06-23 19:39:49 -0700

    default parameters and passing an object as an argument.

--- Python Notes/Classes.py
+++ Python Notes/Classes.py
@@ -98,33 +98,70 @@
 #
 # main()
 
-class BankAccount():
-    def __init__(self, name):
-        self.name = name    #again here, were are setting any new varialbe's "self" to be equle to the name argument given.
-        self.balance = 0.0  #here we initialize the new bank balance at 0.0 float. we would need a parameter if the customer transfers money in
+# class BankAccount():
+#     def __init__(self, name):
+#         self.name = name    #again here, were are setting any new varialbe's "self" to be equle to the name argument given.
+#         self.balance = 0.0  #here we initialize the new bank balance at 0.0 float. we would need a parameter if the customer transfers money in
+#
+#     def show_balance(self):
+#         print(f"{self.name} has a balance of {self.balance} dollars")
+#
+#     def withdraw_money(self, amount):
+#         if amount > self.balance:
+#             print("You don't have the sufficient funds in your account.")
+#         else:
+#             self.balance -= amount
+#             print(f"{self.name} has withdrawn an amount of {amount} dollars and the new balance is {self.balance}")
+#
+#     def deposit_money(self, amount):
+#         self.balance += amount
+#         print(f"{self.name} has deposited {amount} dollars and the total balance is {self.balance}")
+#
+# def main():
+#     savings_account = BankAccount("Olivia")
+#     savings_account.deposit_money(1000)
+#     savings_account.show_balance()
+#     savings_account.withdraw_money(500)
+#     savings_account.withdraw_money(499)
+#     savings_account.withdraw_money(2)
+#     message = savings_account.balance   #This shows that you can create a new variable and have it equal the object and method
+#     print(message)
+#
+# main()
 
-    def show_balance(self):
-        print(f"{self.name} has a balance of {self.balance} dollars")
+# #Testing default parameters
+# class Dog():
+#     def __init__(self, breed="German Shepherd", color="Tan Black"):
+#         self.breed = breed
+#         self.color = color
+#
+#     def dog_breed(self):
+#         print(f"Dog breed is {self.breed}")
+#
+#     def dog_color(self):
+#         print(f"Dog color is {self.color}")
+#
+# def main():
+#     babloo = Dog()  #not giving the dog breed or color so it will default to the default value of german shepherd and tan black.
+#     babloo.dog_breed()
+#     babloo.dog_color()
+#     lucky = Dog("Matlepom", "Tan")  #I added these to show that you can enter your own arguments
+#     lucky.dog_breed()
+#     lucky.dog_color()
+#
+# main()
 
-    def withdraw_money(self, amount):
-        if amount > self.balance:
-            print("You don't have the sufficient funds in your account.")
-        else:
-            self.balance -= amount
-            print(f"{self.name} has withdrawn an amount of {amount} dollars and the new balance is {self.balance}")
+#passing an object as an argument (here singer is the object that was initialized into the Track class)
+class Track:
+    def __init__(self, song, artist):
+        self.song = song
+        self.artist = artist
+        #1 Here we are creating a class that will initialize the object to contain object.song and object.artist
 
-    def deposit_money(self, amount):
-        self.balance += amount
-        print(f"{self.name} has deposited {amount} dollars and the total balance is {self.balance}")
+def print_track(vocalist):
+    print(f"Song is {vocalist.song}")   #2 this function takes in a parameter which later we see will be singer. since vocalist is just a placeholder the real var = singer
+    print(f"Artist is {vocalist.artist}")
 
-def main():
-    savings_account = BankAccount("Olivia")
-    savings_account.deposit_money(1000)
-    savings_account.show_balance()
-    savings_account.withdraw_money(500)
-    savings_account.withdraw_money(499)
-    savings_account.withdraw_money(2)
-    message = savings_account.balance   #This shows that you can create a new variable and have it equal the object and method
-    print(message)
+singer = Track("Family Ties", "Drake")  #1 We initialize the object "singer" which now has two data attributes, song = family ties, artist = drake
 
-main()
+print_track(singer) #3 here we just run the functions which prints singer.song and singer.artist since "vocalist" = singer
